cea7e0f7c791a59b0326ae5c98097a3c811182ef
Luke Secomb <luke.secomb@live.com>
2020-03-04 21:37:43 +1100

    remove '  JSON_FILE_TO_SPLIT: str(),

--- scripts/env.js
+++ scripts/env.js
@@ -3,7 +3,7 @@ import envalid, { str } from 'envalid'
 export const env = envalid.cleanEnv(process.env, {
   SF_DESTINATION: str(),
   SF_SOURCE: str(),
-  JSON_FILE_TO_SPLIT: str(),
+  SF_OBJECT: str(),
   CHUNK_BY_X_RECORDS: str(),
   NAMESPACE: str(),
 })
